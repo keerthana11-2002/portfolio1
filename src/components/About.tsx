b445a330e4b4cfc8f62bfb465052d0900a624e19
Keerthana JK <154956105+keerthana11-2002@users.noreply.github.com>
2024-12-26 11:57:44 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -155,7 +155,7 @@ import { motion } from "framer-motion";
 export default function About() {
   return (
     <div className="py-28">
-    <h2 className="text-4xl sm:text-5xl font-extrabold text-blue-700 mb-6 text-center mt-20">
+    <h2 className="text-4xl sm:text-5xl font-extrabold text-blue-700 mb-6 text-center ">
             Who I Am
           </h2>
     <section className="py-20 bg-gradient-to-b from-gray-50 to-gray-100" id="about">
